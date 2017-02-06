882c99c61c50d07c6d25cb00013704d2a626d710
Benjamin <bdupin@xebia.fr>
2017-02-06 13:26:15 +0100

    Remove Level

--- src/main/java/com/xebia/xke/slack/enums/Level.java
+++ src/main/java/com/xebia/xke/slack/enums/Level.java
@@ -4,36 +4,34 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum Level {
-  EASY(1, "1. Facile"),
-  MEDIUM(2, "2. Moyen"),
-  HARD(3, "3. Guru"),
-  SECOND_PLAYER(-1, "4. Jouer avec un ami");
+	EASY(1, "1. Easy"), MEDIUM(2, "2. Medium"), HARD(3, "3. Guru");
 
-  private int depth;
-  private String description;
-  Level(int depth, String description) {
-    this.depth = depth;
-    this.description = description;
-  }
+	private int depth;
+	private String description;
+	Level(int depth, String description) {
+		this.depth = depth;
+		this.description = description;
+	}
 
-  public int getDepth() {
-    return depth;
-  }
+	public int getDepth() {
+		return depth;
+	}
 
-  public void setDepth(int depth) {
-    this.depth = depth;
-  }
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
 
-  public String getDescription() {
-    return description;
-  }
+	public String getDescription() {
+		return description;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-  public static Level getLevel(int depth) {
-    Optional<Level> optional = Arrays.stream(Level.values()).filter(level -> level.getDepth() == depth).findFirst();
-    return optional.get();
-  }
+	public static Level getLevel(int depth) {
+		Optional<Level> optional = Arrays.stream(Level.values())
+				.filter(level -> level.getDepth() == depth).findFirst();
+		return optional.get();
+	}
 }
